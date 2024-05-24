ddee06767601901438d494fc497e1d7a72d08bb2
Michael Schubert <schubert.michael.j@gmail.com>
2024-05-23 21:33:59 -0500

    Precommit test

--- src/screener/data/client.py
+++ src/screener/data/client.py
@@ -1,9 +1,11 @@
 from decimal import Decimal
 from pydantic import BaseModel
 
+
 class Quote(BaseModel):
     symbol: str
-    price: Decimal 
+    price: Decimal
+
 
 def retrieve_symbol_data(symbol: str, start_date: str, end_date: str) -> Quote:
-
+    return
